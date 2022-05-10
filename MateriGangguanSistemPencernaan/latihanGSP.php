0d8ffb6268a933fad26049245398d29d92aab91e
Jimmi Eduard Simangunsong <jimmi24eduard@gmail.com>
2022-05-10 10:08:42 +0700

    added exercise GSP

--- MateriGangguanSistemPencernaan/latihanGSP.php
+++ MateriGangguanSistemPencernaan/latihanGSP.php
@@ -1,1 +1,52 @@
-ini latihan ygy
+
+
+
+    <div class="start_btn"><button id="tombol">Mulai Latihan</button></div>
+    
+      
+        <!-- Tempat ini isi dari lembar latihan -->
+
+    
+
+   
+    
+    
+
+         
+    
+    <script type="text/javascript">
+       $(document).ready(function () {
+
+      $('.home').click(function (e) { 
+          e.preventDefault();
+
+          var menu =  $(this).find('a').attr('class');
+          console.log(menu)
+
+          if(menu == "wrap"){
+              $('#konten').load('home.php');
+            }
+
+      });
+    }); 
+
+    </script>
+
+
+<script type="text/javascript">
+       $(document).ready(function () {
+
+      $('.start_btn').click(function (e) { 
+          e.preventDefault();
+
+          var menu =  $(this).find('button').attr('id');
+          console.log(menu)
+
+          if(menu == "tombol"){
+              $('.isimateri2').load('latihan/lembarlatihanGSP.php');
+            }
+
+      });
+    }); 
+
+    </script>
